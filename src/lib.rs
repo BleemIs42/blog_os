4ce28fb2e2e677dc41269a679f374332b8e6012b
Shuanghui Yan <shuanghui.yan@thoughtworks.com>
2021-05-06 20:02:08 +0800

    Add interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 pub mod vga_buffer;
 pub mod serial;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -43,8 +45,13 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
 
 #[cfg(test)]
