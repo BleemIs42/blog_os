53eee0555751ef854a3aef585a8f723f43566403
Shuanghui Yan <shuanghui.yan@thoughtworks.com>
2021-07-16 14:42:09 +0800

    add hello world

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
 // 默认命名为 `_start`
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
